c4f84825dd605761836db060be93623587eca011
amina <ip204@git.scc>
2022-12-03 21:53:34 +0300

    подключение к базе данных

--- SportStore/MainWindow.xaml.cs
+++ SportStore/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SportStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,12 @@ namespace SportStore
         public MainWindow()
         {
             InitializeComponent();
+
+            using (SportStoreContext db = new SportStoreContext())
+            {
+                var a = db.Users.FirstOrDefault();
+                MessageBox.Show("Db connect");
+            }
         }
     }
 }
